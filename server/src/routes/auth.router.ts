01ea221453f32803910e9dbc71f065981142f453
romanthapa2 <thaparoman970@gmail.com>
2024-10-15 20:27:14 +0545

    auth router setup

--- server/src/routes/auth.router.ts
+++ server/src/routes/auth.router.ts
@@ -1,18 +1,34 @@
 import { Router } from 'express';
 import {
-  httpGithubAccessToken,
-  httpGithubFinalSteps,
-  httpGoogleAccessToken,
-  httpGoogleFinalSteps,
+  
+  GithubAccessToken,
+  GithubFinalSteps,
+  GoogleAccessToken,
+  GoogleFinalSteps,
+  CreateAccount,
+  Login,
+  Logout,
+  ChangePassword,
+  ChangeUsername
 } from '../controllers/auth.controller';
 
 const authRouter = Router();
 
-authRouter.get('/github/access-token', httpGithubAccessToken);
-authRouter.post('/github/final-steps', httpGithubFinalSteps);
+authRouter.get('/github/access-token', GithubAccessToken);
+authRouter.post('/github/final-steps', GithubFinalSteps);
 
 
-authRouter.get('/google/access-token', httpGoogleAccessToken);
-authRouter.post('/google/final-steps', httpGoogleFinalSteps);
+authRouter.get('/google/access-token', GoogleAccessToken);
+authRouter.post('/google/final-steps', GoogleFinalSteps);
+
+
+authRouter.post('/create-account', CreateAccount);
+authRouter.post('/login', Login);
+authRouter.post('/logout', Logout);
+
+authRouter.post('/change-username', ChangeUsername);
+authRouter.post('/change-password', ChangePassword);
+
+
 
 export default authRouter;
